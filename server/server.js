35f84b9f37377544bda0101772be5531807a73d5
Stephen <stephen_1996@live.ca>
2022-11-26 22:42:02 -0800

    created controller, query, and routes for users

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send(
-  'working'
-));
+app.get('/', (req, res) => 
+  res.send('working')
+);
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+app.listen(4000, () => console.log('Server running on port 4000'));
