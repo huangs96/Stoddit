3bb7495cc4000edf8d4b8c3b095dca3e49e48f8f
Stephen <stephen_1996@live.ca>
2022-11-28 14:39:23 -0800

    changed userExists to userPhoneExists and usernameExists

--- server/controllers/register.controller.js
+++ server/controllers/register.controller.js
@@ -25,17 +25,24 @@ const getUsersById = (req, res) => {
 const registerUser = (req, res) => {
   const {username, password, bio, phone} = req.body;
   
-  //check if username exists
-  client.query(queries.userExists, [phone], (err, results) => {
+  //check if phone exists
+  client.query(queries.userPhoneExists, [phone], (err, results) => {
     if (results.rows.length) {
       return res.send('Phone number has already been registered.');
     }
-  });
-
-  //register user
-  client.query(queries.addUser, [username, password, bio,phone], (err, results) => {
-    if (err) throw err;
-    res.status(201).send('Your account has been created!');
+    
+    //check if username exists
+    client.query(queries.usernameExists, [username], (err, results) => {
+      if (results.rows.length) {
+        return res.send('Username has already been taken.');
+      }
+      
+      //register user
+      client.query(queries.addUser, [username, password, bio,phone], (err, results) => {
+        if (err) throw err;
+        res.status(201).send('Your account has been created!');
+      });
+    });
   });
 };
 
