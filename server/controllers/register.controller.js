2751e2da22cb470c15e84ee6ed9a01d366caa667
Stephen <stephen_1996@live.ca>
2022-11-28 22:33:19 -0800

    refactored all controller files

--- server/controllers/register.controller.js
+++ server/controllers/register.controller.js
@@ -6,46 +6,29 @@ const registerPage = (req, res) => {
   res.send('register page working');
 }
 
-const getUsers = ((req, res) => {
-  client.query(queries.getUsers, (err, results) => {
-    if (err) throw err;
-    console.log(results);
-    res.status(200).json(results.rows);
-  })
-});
-
-const getUsersById = (req, res) => {
-  const id = parseInt(req.params.id);
-  client.query(queries.getUsersById, [id], (err, results) => {
-    if (err) throw err;
-    res.status(200).json(results.rows);
-  });
-};
-
 const registerUser = async (req, res) => {
   const {username, password, bio, phone} = req.body;
   const encryptedPassword = await bcrypt.hash(password, 10);
 
   try {
     //check if phone exists
-  client.query(queries.userPhoneExists, [phone], (err, results) => {
-    if (results.rows.length) {
-      return res.send('Phone number has already been registered.');
-    }
-    
-    //check if username exists
-    client.query(queries.usernameExists, [username], (err, results) => {
-      if (results.rows.length) {
-        return res.send('Username has already been taken.');
-      }
+    const userPhoneExists = await client.query(queries.userPhoneExists, [phone]);
+    if (userPhoneExists.rows.length) {
+      return res.send('Phone number is in use.');
+    };
 
-      //register user
-      client.query(queries.addUser, [username, encryptedPassword, bio, phone], (err, results) => {
-        if (err) throw err;
-        res.status(201).send('Your account has been created!');
-      });
-    });
-  });
+    //check if username exists
+    const usernameExists = await client.query(queries.usernameExists, [username]);
+    console.log(usernameExists);
+    if (usernameExists.rows.length) {
+      return res.send('Username has already been taken.')
+    };
+
+    //register user if phone & username are not taken
+    const registerUser = await client.query(queries.addUser, [username, encryptedPassword, bio, phone]);
+    if (registerUser) {
+      res.status(201).send('Your account has been created!');
+    };
   } catch (err) {
     console.log(err);
     return res.json({status:'error'});
@@ -54,7 +37,5 @@ const registerUser = async (req, res) => {
 
 module.exports = {
   registerPage,
-  getUsers,
-  getUsersById,
   registerUser,
 } 
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -3,7 +3,6 @@ const controller = require('../controllers/user.controller');
 const router = Router();
 
 router.get('/', controller.getUsers);
-router.post('/', controller.registerUser);
 router.get('/:id', controller.getUsersById);
 router.put('/:id', controller.updateUser);
 router.delete('/:id', controller.deleteUser);
