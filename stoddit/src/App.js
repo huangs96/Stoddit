bdf4b2ee77c04dba9cb985fbf150f191aff9d3ad
Stephen <stephen_1996@live.ca>
2022-12-01 14:26:07 -0800

    created login page, implementing JWT cookie storage

--- stoddit/src/App.js
+++ stoddit/src/App.js
@@ -1,8 +1,12 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import react, { useState } from 'react';
 import './App.css';
 import LoginPage from './pages/Login/LoginIndex';
+// import LoginForm from './pages/Login/LoginForm'
+
 
 function App() {
+
   return (
     <div className="App">
       <LoginPage />
--- stoddit/src/pages/Login/LoginIndex.js
+++ stoddit/src/pages/Login/LoginIndex.js
@@ -1,51 +1,49 @@
 import React, { useState, useEffect } from 'react';
 import LoginForm from './LoginForm';
-import axios from 'axios';
+// import axios from 'axios';
+
+//setting User Token and storing in local storage
+function setToken(userToken) {
+  sessionStorage.setItem('token', JSON.stringify(userToken));
+  const userToken = JSON.parse(tokenString);
+  return userToken?.token
+}
 
 function LoginPage() {
 
-  const fetchApi = async () => {
-    try {
-      const res = await axios.get('http://localhost:5000/login');
-      console.log(res);
-    } catch (err) {
-      console.log(err.message);
-    }
-  }
-
-  useEffect(() => {
-    fetchApi()
-  }, [])
-
-  const adminUser = {
-    username: "steve@email.com",
-    password: "steve"
-  }
-
+  const [token, setToken] = useState();
   const [user, setUser] = useState({username: "", password:""});
   const [error, setError] = useState("");
 
-  const Login = details => {
-    if (details.username === adminUser.username && details.password === adminUser.password) {
-      setUser({
-        username: details.username,
-        password: details.password
-      })
-    } else {
-      console.log('Username or password is incorrect.')
-    }
-  };
 
-  const Logout = details => {
-    console.log('loggedout');
-    setUser({username: "", password:""});
-  }
+  // const Login = details => {
+  //   console.log('login1', user);
+  //   console.log('details', details.username);
+  //   setUser({username: details.username, password: details.password});
+    // if (details.username === adminUser.username && details.password === adminUser.password) {
+    //   setUser({
+    //     username: details.username,
+    //     password: details.password
+    //   })
+    // } else {
+    //   console.log('Username or password is incorrect.')
+    // }
+  // };
+
+  // const Logout = details => {
+  //   console.log('loggedout');
+  //   setUser({username: "", password:""});
+  // }
+
+  if(!token) {
+    return <LoginPage setToken={setToken} />
+  };
   
   return (
     <div className="loginPage">
       {(user.username != "") ? (
         <div className="welcomeMessage">
-          <h2>Welcome, <span>{user.name}</span></h2>
+          <h2>Welcome, <span>{user.username}</span></h2>
           <button onClick={Logout}>Logout</button>
         </div>
       ) : (
