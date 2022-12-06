5364037ec6524d49f7b5e193796d0d4e2d08d8ab
Stephen <stephen_1996@live.ca>
2022-12-06 15:24:40 -0800

    fixed avatar picture size

--- stoddit/src/pages/Chat/chatOnline/FriendsOnline.jsx
+++ stoddit/src/pages/Chat/chatOnline/FriendsOnline.jsx
@@ -13,22 +13,6 @@ function FriendsOnline() {
           <div className="chatOnlineBadge"></div>
         </div>
           <span className="onlineFriendName">Kurt Galvin</span>
-        <div className="friendOnlineImgContainer">
-          <img 
-          className="friendsOnlineImg"
-          src="https://blob.sololearn.com/avatars/587fc1dd-3595-4fa0-94b8-6cf4787ce36d.jpg" alt=""
-          />
-          <div className="chatOnlineBadge"></div>
-        </div>
-          <span className="onlineFriendName">Kurt Galvin</span>
-        <div className="friendOnlineImgContainer">
-          <img 
-          className="friendsOnlineImg"
-          src="https://blob.sololearn.com/avatars/587fc1dd-3595-4fa0-94b8-6cf4787ce36d.jpg" alt=""
-          />
-          <div className="chatOnlineBadge"></div>
-        </div>
-          <span className="onlineFriendName">Kurt Galvin</span>
       </div>
     </div>
   );
