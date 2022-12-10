9c028b23448ab2121cbf6277dd163f858c96391c
Stephen <stephen_1996@live.ca>
2022-12-09 23:15:17 -0800

    working on messages function

--- stoddit/src/pages/Chat/Messages/Message.jsx
+++ stoddit/src/pages/Chat/Messages/Message.jsx
@@ -3,37 +3,22 @@ import React, { useState, useEffect } from 'react';
 import { getMessages } from '../../../services/chat.service';
 
 function Message({messages, userID}) {
-  console.log('message in conversation', messages);
-  const getMessageData = getMessages();
-  // console.log('messagedata----', getMessageData);
-  // console.log('messageuserID---', userID);
-  const displayMessage = getMessageData.map((data, i) => {
-    if(messages.length > 0) {
-      messages.map(propData => {
-        //extract propData values into global variable to display
-        if (propData === data.text) {
-          if(userID === data.id) {
-            data.ownMessage = true;
-          } else {
-            data.ownMessage = false;
-          };
-        }
-      })
-    };
+  let mappedGetMessages = [];
 
-    return (
-      <div className={data.ownMessage ? "message own" : "message"}>
-      <div className="messageTop">
-        <img 
-          className="messageImg" 
-          src={data.img} 
-          alt=""
-        />
-        <p className="messageText" key={i}>{messages}</p>
-      </div>
-      <div className="messageBottom"key={i}>{data.date}</div>
-    </div>
-    )
+  const getMessageData = getMessages();
+  const displayMessage = getMessageData.map((data, i) => {
+    if(messages.length < 0) {
+      console.log('no messages');
+    }
+    for (let i=0; i<messages.length; i++) {
+      let value = messages[i];
+    }
+    // console.log(value);
+    if(userID === data.id) {
+      data.ownMessage = true;
+    } else {
+      data.ownMessage = false;
+    };
   });
 
   
@@ -41,8 +26,17 @@ function Message({messages, userID}) {
   },[])
 
   return (
+    // <div className={data.ownMessage ? "message own" : "message"}>
     <div>
-      {displayMessage}
+      <div className="messageTop">
+        <img 
+          className="messageImg" 
+          src="" 
+          alt=""
+        />
+        <p className="messageText" key={messages.length}>{messages}</p>
+      </div>
+      <div className="messageBottom"key={messages.length}>{'data.date'}</div>
     </div>
   )
 }
