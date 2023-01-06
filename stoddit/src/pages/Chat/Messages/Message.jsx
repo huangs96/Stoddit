5f1be42415f0c94ec37f68eef5900c00edd43ba9
Stephen <stephen_1996@live.ca>
2023-01-06 01:24:44 -0800

    displaying username in chat with multiple users in chatroom

--- stoddit/src/pages/Chat/Messages/Message.jsx
+++ stoddit/src/pages/Chat/Messages/Message.jsx
@@ -20,6 +20,9 @@ function Message({userID, messages}) {
           return (
             <div className={data.ownMessage ? "message own" : "message"}> 
               <div className="messageTop">
+              <h6>
+                {data.username}
+              </h6>
               <img 
                 className="messageImg" 
                 src='https://as2.ftcdn.net/v2/jpg/03/49/49/79/1000_F_349497933_Ly4im8BDmHLaLzgyKg2f2yZOvJjBtlw5.jpg'
