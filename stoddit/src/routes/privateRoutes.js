be61155d3a13303dcf540363905c10ebacc12a33
Stephen <stephen_1996@live.ca>
2023-01-27 03:11:44 -0800

    set up private route

--- stoddit/src/routes/privateRoutes.js
+++ stoddit/src/routes/privateRoutes.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Outlet, Navigate } from 'react-router-dom';
 
 function PrivateRoutes() {
+  const userID = JSON.parse(localStorage.getItem('UserID'));
+  // const username = JSON.parse(localStorage.getItem('Username'));
+
   return (
-    <div>privateRoute</div>
+    userID ? <Outlet /> : <Navigate to='/login' />
   )
 }
 
