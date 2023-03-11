55378c85b5ef10882292fdda58a739f166ad4f8e
Stephen <stephen_1996@live.ca>
2023-03-11 02:46:37 -0800

    added profile field forms

--- stoddit/src/pages/Profile/ProfileIndex.jsx
+++ stoddit/src/pages/Profile/ProfileIndex.jsx
@@ -1,9 +1,53 @@
 import React from 'react'
+import { TextField, MenuItem, Typography, Grid, Button } from '@mui/material';
+import './ProfileIndex.css';
 
 function ProfileIndex() {
+
   return (
-    <div>ProfileIndex</div>
-  )
-}
+    <>
+      <form className='accountFormContainer' noValidate autoComplete="off">
+        <div className='accountForm'>
+          <Typography className='accountTitleText'>
+            {" "}
+            ACCOUNT INFORMATION{" "}
+          </Typography>
+          <TextField
+            id="standard-required"
+            label="First Name"
+            style={{
+              width: "97%"
+            }}
+          />
+          <TextField
+            label="Last Name"
+            style={{
+              width: "97%"
+            }}
+          />
+          <TextField
+            id="standard-required"
+            label="Phone Number"
+            fullWidth={true}
+            type="number"
+            style={{
+              width: "97%"
+            }}
+          />
+          <TextField
+            id="standard-required"
+            label="Address"
+            style={{
+              width: "97%"
+            }}
+          />
+        </div>
+      </form>
+      <Button className="submitButton" color="primary">
+        Save Changes
+      </Button>
+    </>
+  );
+};
 
 export default ProfileIndex
