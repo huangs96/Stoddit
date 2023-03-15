0eff258b6703595c83f2afe17430029969e56bad
Stephen <stephen_1996@live.ca>
2023-03-15 01:22:20 -0700

    added multer middleware to post image route for multidata forms

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -2,6 +2,8 @@ const { Router } = require('express');
 const controller = require('../controllers/user.controller');
 const router = Router();
 const auth = require('../middleware/verifyToken');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' })
 
 router.get('/', controller.getUsers);
 router.get('/home', auth.authToken, controller.getUserHomePage);
