45a79fbe247460476b7acd0c90d967e3f0d8090e
Stephen <stephen_1996@live.ca>
2023-03-18 23:26:46 -0700

    created AwsS3 class for anything S3 related. Creating methods now.

---
+++ server/classes/awsClass.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const { S3Client, PutObjectCommand, GetObjectCommand } = require("@aws-sdk/client-s3");
+
+const bucketName = process.env.BUCKET_NAME;
+const bucketRegion = process.env.BUCKET_REGION;
+const accessKey = process.env.AWS_ACCESS_KEY_ID;
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+
+const s3 = new S3Client({
+  credentials: {
+    accessKeyId: accessKey,
+    secretAccessKey: secretAccessKey
+  },
+  region: bucketRegion
+});
+
+class AwsS3 {
+  constructor() {
+
+  };
+
+  getImgUrl = () => {
+    const getObjectParams = {
+      Bucket: bucketName,
+      Key: `Stoddit-Profile-Images/${userDetails.contact_img}`
+    };
+
+    const command = new GetObjectCommand(getObjectParams);
+    const url = await getSignedUrl(s3, command, { expiresIn: 3600 });
+  };
+};
