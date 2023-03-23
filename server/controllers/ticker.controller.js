5f10fe5e66892231fcb34ac51605280e184f5807
Stephen <stephen_1996@live.ca>
2023-03-23 16:04:27 -0700

    initiated get ticker by chatroom

--- server/controllers/ticker.controller.js
+++ server/controllers/ticker.controller.js
@@ -13,6 +13,17 @@ const getTickers = (async (req, res) => {
   };
 });
 
+const getTickersByChatroomID = (async (req, res) => {
+  try {
+    const allTickers = await client.query(queries.getAllTickers);
+    if (allTickers.rows.length) {
+      res.status(200).json(allTickers.rows);
+    };
+  } catch (err) {
+    return res.status(400).send(err);
+  };
+});
+
 module.exports = {
   getTickers
-}
+};
