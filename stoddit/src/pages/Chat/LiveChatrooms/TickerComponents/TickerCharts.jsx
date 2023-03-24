2f9637eaaa484a9ae46bece14c101ae635d1654c
Stephen <stephen_1996@live.ca>
2023-03-23 23:41:00 -0700

    installed chartJS 2

--- stoddit/src/pages/Chat/LiveChatrooms/TickerComponents/TickerCharts.jsx
+++ stoddit/src/pages/Chat/LiveChatrooms/TickerComponents/TickerCharts.jsx
@@ -1,18 +1,36 @@
 import React from 'react';
-import { LineController, LineElement, PointElement} from 'chart.js';
+import {
+  Chart as ChartJS,
+  CategoryScale, //x-axis
+  LinearScale, //y-axis
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+const chart = new Chart({
+  type: 'line',
+  data: data,
+  options: {
+  }
+});
 
 function TickerCharts({ticker}) {
-  console.log('chart', LineController, LineElement, PointElement);
+  // console.log('chart', LineController, LineElement, PointElement);
   return (
-    <div 
-      className="friendsOffline"
-      >
-      <div className="chatOfflineFriend">
-        <div className="friendOfflineImgContainer">
-        </div>
-        <span className="offlineFriendName">{ticker.name}</span>
-      </div>
-    </div>
+    // <div 
+    //   className="friendsOffline"
+    //   >
+    //   <div className="chatOfflineFriend">
+    //     <div className="friendOfflineImgContainer">
+    //     </div>
+    //     <span className="offlineFriendName">{ticker.name}</span>
+    //   </div>
+    // </div>
+    {chart}
   )
 }
 
