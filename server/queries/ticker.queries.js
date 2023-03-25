1253d0748ab4fb8e23224b3857ed9034f70962d4
Stephen <stephen_1996@live.ca>
2023-03-24 18:42:45 -0700

    changed api to send pricing as well

--- server/queries/ticker.queries.js
+++ server/queries/ticker.queries.js
@@ -1,5 +1,5 @@
 const getAllTickers = 'SELECT * FROM ticker;';
-const getTickersByChatroomID = 'SELECT * FROM ticker WHERE chatroom_id = $1;';
+const getTickersByChatroomID = 'SELECT * FROM ticker INNER JOIN ticker_time_interval ON ticker.id=ticker_time_interval.ticker_id WHERE chatroom_id = $1;';
 
 module.exports = {
   getAllTickers,
