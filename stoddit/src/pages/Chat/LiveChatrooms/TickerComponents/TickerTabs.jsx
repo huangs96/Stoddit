77cf0e53e4e92c3c5bc6adbd2dc33b6231689882
Stephen <stephen_1996@live.ca>
2023-03-24 23:55:51 -0700

    styling ticker tab

--- stoddit/src/pages/Chat/LiveChatrooms/TickerComponents/TickerTabs.jsx
+++ stoddit/src/pages/Chat/LiveChatrooms/TickerComponents/TickerTabs.jsx
@@ -1,34 +1,37 @@
 import React from 'react';
-import TickerTabs from './TickerTabs.css';
+import '/TickerTabs.css';
+
+// import chevronUp from '../../images/chevron-up.svg';
+// import chevronDown from '../../images/chevron-down.svg';
 
 function TickerTabs({ticker}) {
   console.log('tickertab', ticker);
   return (
-  //   <div className="stockContainer">
-  //   <div className="stockList">
-  //     <span className="stockName">{ticker.name}</span>
-  //     <span className="stockSymbol">{ticker.symbol}</span>
-  //     <span className="stockCPrice">{ticker.current_price}</span>
-  //     <span className="stockHPrice">{ticker.high_price}</span>
-  //     <span className="stockLPrice">{ticker.low_price}</span>
-  //     <span className="stockRecommendation">{ticker.recommendation}</span>
-  //     <span className="stockVolume">{ticker.volume}</span>
-  //   </div>
-  // </div>
-  <div data-id={props.id} className={[stylesGeneric.tickerItem, stylesFinancial.tickerItemFinancial].join(' ')}>
-    <div className={stylesFinancial.tickerTopLevel}>
-    {ticker.name}
-      <div className={stylesFinancial.tickerSymbol}>{props.symbol}</div>
-      <div className={stylesFinancial.lastPrice}>{props.lastPrice}</div>
-    </div>
-    <div className={stylesFinancial.tickerStats}>
-      <img  className={stylesFinancial.tickerMovement} src={props.change ? chevronUp : chevronDown} alt={props.change ? 'up' : 'down'} />
-      <div className={[statusClassName, stylesFinancial.tickerInfo].join(' ')}>
-        <div className={stylesFinancial.tickerChangePercentage}>{props.percentage}</div>
-        <div className={stylesFinancial.tickerCurrentPrice}>{props.currentPrice}</div>
-      </div>
+    <div className="stockContainer">
+    <div className="stockList">
+      <div className="stockName">{ticker.name}</div>
+      <div className="stockSymbol">{ticker.symbol}</div>
+      <div className="stockCPrice">{ticker.current_price}</div>
+      <div className="stockHPrice">{ticker.high_price}</div>
+      <div className="stockLPrice">{ticker.low_price}</div>
+      <div className="stockRecommendation">{ticker.recommendation}</div>
+      <div className="stockVolume">{ticker.volume}</div>
     </div>
   </div>
+  // <div data-id={props.id} className={[stylesGeneric.tickerItem, stylesFinancial.tickerItemFinancial].join(' ')}>
+  //   <div className={stylesFinancial.tickerTopLevel}>
+  //   {ticker.name}
+  //     <div className={stylesFinancial.tickerSymbol}>{props.symbol}</div>
+  //     <div className={stylesFinancial.lastPrice}>{props.lastPrice}</div>
+  //   </div>
+  //   <div className={stylesFinancial.tickerStats}>
+  //     <img  className={stylesFinancial.tickerMovement} src={props.change ? chevronUp : chevronDown} alt={props.change ? 'up' : 'down'} />
+  //     <div className={[statusClassName, stylesFinancial.tickerInfo].join(' ')}>
+  //       <div className={stylesFinancial.tickerChangePercentage}>{props.percentage}</div>
+  //       <div className={stylesFinancial.tickerCurrentPrice}>{props.currentPrice}</div>
+  //     </div>
+  //   </div>
+  // </div>
   )
 }
 
