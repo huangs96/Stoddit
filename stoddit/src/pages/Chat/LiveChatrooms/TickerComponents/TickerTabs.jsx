3e6310756ae4c76c4c8f8d53a8fc14f7c0f926b9
Stephen <stephen_1996@live.ca>
2023-03-25 13:51:52 -0700

    doing CSS for ticker

--- stoddit/src/pages/Chat/LiveChatrooms/TickerComponents/TickerTabs.jsx
+++ stoddit/src/pages/Chat/LiveChatrooms/TickerComponents/TickerTabs.jsx
@@ -9,26 +9,30 @@ function TickerTabs({ticker}) {
   return (
     <div className="tickerContainer">
       <div className="tickerList">
-        <div className="tickerName">{ticker.name}</div>
-        <div className="tickerSymbol">{ticker.symbol}</div>
-        <div className="tickerCPrice">{ticker.current_price}</div>
-        {/* <div className="tickerHPrice">{ticker.high_price}</div> */}
-        <img
-          src={tickerChange === 'positive' ?
-            chevronUp
-            :
-            chevronDown
-          }
-          alt={tickerChange === 'negative' ?
-            chevronUp
-            :
-            chevronDown
-          }
-        >
-        </img>
-        {/* <div className="tickerLPrice">{ticker.low_price}</div> */}
-        <div className="tickerRecommendation">{ticker.recommendation}</div>
-        <div className="tickerVolume">{ticker.volume}</div>
+        <div className="tickerNameSymbolContainer">
+          <div className="tickerName">{ticker.name}</div>
+          <div className="tickerSymbol">{ticker.symbol}</div>
+        </div>
+        <div className="priceContainer">
+          <div className="tickerCPrice">{ticker.current_price}</div>
+          {/* <div className="tickerHPrice">{ticker.high_price}</div> */}
+          <img
+            src={tickerChange === 'positive' ?
+              chevronUp
+              :
+              chevronDown
+            }
+            alt={tickerChange === 'negative' ?
+              chevronUp
+              :
+              chevronDown
+            }
+          >
+          </img>
+          {/* <div className="tickerLPrice">{ticker.low_price}</div> */}
+          <div className="tickerRecommendation">{ticker.recommendation}</div>
+          <div className="tickerVolume">{ticker.volume}</div>
+        </div>
       </div>
     </div>
   )
