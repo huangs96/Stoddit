64fbafe1f31a703eb74c75fff7a12b640bc16ee4
Stephen <stephen_1996@live.ca>
2023-03-25 13:38:12 -0700

    changed setState to useState

--- stoddit/src/pages/Chat/LiveChatrooms/TickerComponents/TickerTabs.jsx
+++ stoddit/src/pages/Chat/LiveChatrooms/TickerComponents/TickerTabs.jsx
@@ -1,10 +1,10 @@
-import React, {setState} from 'react';
+import React, {useState} from 'react';
 import './TickerTabs.css';
 import chevronDown from '../../../../images/chevron-down.svg'
 import chevronUp from '../../../../images/chevron-up.svg';
 
 function TickerTabs({ticker}) {
-  const [tickerChange, setTickerChange] = setState(null);
+  const [tickerChange, setTickerChange] = useState('positive');
   console.log('tickertab', ticker);
   return (
     <div className="tickerContainer">
@@ -12,21 +12,21 @@ function TickerTabs({ticker}) {
         <div className="tickerName">{ticker.name}</div>
         <div className="tickerSymbol">{ticker.symbol}</div>
         <div className="tickerCPrice">{ticker.current_price}</div>
-        <div className="tickerHPrice">{ticker.high_price}</div>
+        {/* <div className="tickerHPrice">{ticker.high_price}</div> */}
         <img
-          src={tickerChange ?
+          src={tickerChange === 'positive' ?
             chevronUp
             :
             chevronDown
           }
-          alt={tickerChange ?
+          alt={tickerChange === 'negative' ?
             chevronUp
             :
             chevronDown
           }
         >
         </img>
-        <div className="tickerLPrice">{ticker.low_price}</div>
+        {/* <div className="tickerLPrice">{ticker.low_price}</div> */}
         <div className="tickerRecommendation">{ticker.recommendation}</div>
         <div className="tickerVolume">{ticker.volume}</div>
       </div>
