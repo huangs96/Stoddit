73413ecd1efc8fa11e20cc279114e2d7312ce71b
Stephen <stephen_1996@live.ca>
2023-03-25 16:51:46 -0700

    planning how to sort sockets and real time stock movement

--- stoddit/src/pages/Chat/LiveChatrooms/TickerComponents/TickerTabs.jsx
+++ stoddit/src/pages/Chat/LiveChatrooms/TickerComponents/TickerTabs.jsx
@@ -6,6 +6,7 @@ import chevronUp from '../../../../images/chevron-up.svg';
 function TickerTabs({ticker}) {
   const [tickerChange, setTickerChange] = useState('positive');
   console.log('tickertab', ticker);
+  const changePercentage = ticker.changePercentage;
   return (
     <div className="tickerContainer">
       <div className="tickerList">
@@ -14,23 +15,24 @@ function TickerTabs({ticker}) {
           <div className="tickerSymbol">{ticker.symbol}</div>
         </div>
         <div className="priceContainer">
-          <div className="tickerCPrice">{ticker.current_price}</div>
+          <div className="changePercentage"></div>
           {/* <div className="tickerHPrice">{ticker.high_price}</div> */}
           <img
             src={tickerChange === 'positive' ?
-              chevronUp
-              :
-              chevronDown
-            }
+            chevronUp
+            :
+            chevronDown
+          }
             alt={tickerChange === 'negative' ?
-              chevronUp
-              :
-              chevronDown
-            }
+            chevronUp
+            :
+            chevronDown
+          }
           >
           </img>
+          <div className="tickerCPrice">{ticker.current_price}</div>
           {/* <div className="tickerLPrice">{ticker.low_price}</div> */}
-          <div className="tickerRecommendation">{ticker.recommendation}</div>
+          {/* <div className="tickerRecommendation">{ticker.recommendation}</div> */}
           <div className="tickerVolume">{ticker.volume}</div>
         </div>
       </div>
