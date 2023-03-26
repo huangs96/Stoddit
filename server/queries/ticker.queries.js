cf99ab1c334c004b3282503a9a433f464f118af2
Stephen <stephen_1996@live.ca>
2023-03-25 22:33:43 -0700

    changed query to sort through date_time. May not be the most efficient way to store stream of data but this will work for now.

--- server/queries/ticker.queries.js
+++ server/queries/ticker.queries.js
@@ -1,6 +1,7 @@
 const getAllTickers = 'SELECT * FROM ticker;';
-const getTickersByChatroomID = 'SELECT * FROM ticker INNER JOIN ticker_time_interval ON ticker.id=ticker_time_interval.ticker_id WHERE chatroom_id = $1;';
+const getTickersByChatroomID = 'SELECT * FROM ticker INNER JOIN ticker_time_interval ON ticker.id=ticker_time_interval.ticker_id WHERE chatroom_id = $1 ORDER BY date_time DESC;';
 const getTickersByTimeInterval = '';
+const getTickersByTickerID = '';
 
 module.exports = {
   getAllTickers,
