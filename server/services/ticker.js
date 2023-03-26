afeb832f6fb46f376bcd8d0fbaeb374b50db8079
Stephen <stephen_1996@live.ca>
2023-03-26 02:59:57 -0700

    ticker change business service function complete.

--- server/services/ticker.js
+++ server/services/ticker.js
@@ -7,7 +7,6 @@ const tickerAdd = () => {
 
 const tickerChange = (tickerData) => {
   const mostRecentTickers = {};
-  // console.log('tickerData in bizz logic', tickerData);
   for (let ticker = 0; ticker<tickerData.length; ticker++) {
     let eachTickerInfo = tickerData[ticker];
     if (!mostRecentTickers[eachTickerInfo.name]) {
@@ -16,8 +15,6 @@ const tickerChange = (tickerData) => {
       mostRecentTickers[eachTickerInfo.name].push(eachTickerInfo);
     };
   };
-  // return mostRecentTickers;
-  // console.log('mostRecentTickers11111', mostRecentTickers);
   let mostRecentTickerData = Object.values(mostRecentTickers);
   for (let recentTicker = 0; recentTicker<mostRecentTickerData.length; recentTicker++) {
     let firstRecentTicker = mostRecentTickerData[recentTicker][0];
@@ -26,10 +23,10 @@ const tickerChange = (tickerData) => {
     const currentPriceChangePercentage = Math.round(((firstRecentTicker.current_price - secondRecentTicker.current_price) / secondRecentTicker.current_price) * 100).toFixed(0);
     if (mostRecentTickers.hasOwnProperty(firstRecentTicker.name)) {
       mostRecentTickers[firstRecentTicker.name].push(currentPriceChange);
-      mostRecentTickers[firstRecentTicker.name].push(currentPriceChangePercentage);
+      mostRecentTickers[firstRecentTicker.name].push(parseInt(currentPriceChangePercentage));
     };
   };
-  // console.log('ticker222222', mostRecentTickers);
+  return mostRecentTickers;
 };
 
 module.exports = {
