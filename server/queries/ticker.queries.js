d052e33d7d5297af928879b60208010415e0bdda
Stephen <stephen_1996@live.ca>
2023-03-25 21:57:23 -0700

    changing database timestamp to timestamp data type for ticker_time_interval

--- server/queries/ticker.queries.js
+++ server/queries/ticker.queries.js
@@ -1,5 +1,6 @@
 const getAllTickers = 'SELECT * FROM ticker;';
 const getTickersByChatroomID = 'SELECT * FROM ticker INNER JOIN ticker_time_interval ON ticker.id=ticker_time_interval.ticker_id WHERE chatroom_id = $1;';
+const getTickersByTimeInterval = '';
 
 module.exports = {
   getAllTickers,
