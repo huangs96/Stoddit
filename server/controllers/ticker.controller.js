49b6ae11ed9dd036b7480334412e29fb0d4b963a
Stephen <stephen_1996@live.ca>
2023-03-25 22:55:55 -0700

    initiated getting most recent ticker data in service file of ticker server

--- server/controllers/ticker.controller.js
+++ server/controllers/ticker.controller.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const client = require('../classes/pgPoolClass');
 const queries = require('../queries/ticker.queries');
+const tickerLogic = require('../services/ticker');
 
 const getTickers = (async (req, res) => {
   try {
@@ -18,7 +19,7 @@ const getTickersByChatroomID = (async (req, res) => {
   console.log('id', chatroom_id);
   try {
     const allTickers = await client.query(queries.getTickersByChatroomID, [chatroom_id]);
-    console.log('allTickers', allTickers.rows);
+    tickerLogic.tickerChange(allTickers.rows);
     if (allTickers) {
       res.status(200).json(allTickers.rows);
     };
--- server/services/ticker.js
+++ server/services/ticker.js
@@ -1,3 +1,16 @@
-const tickerChange = (tickerData) => {
-  
+const client = require('../classes/pgPoolClass');
+const queries = require('../queries/ticker.queries');
+
+const tickerAdd = () => {
+
 };
+
+const tickerChange = (tickerData) => {
+  console.log('tickerData in bizz logic', tickerData);
+  const recentTickerData = [tickerData[0], tickerData[1]];
+  console.log('recentTickerData', recentTickerData);
+};
+
+module.exports = {
+  tickerChange
+}
