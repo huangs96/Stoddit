95e18d7954b31aa5cc488eaef1c661b9298f2769
Stephen <stephen_1996@live.ca>
2023-03-26 17:48:51 -0700

    initiated insertTickerTimeInterval function

--- server/controllers/ticker.controller.js
+++ server/controllers/ticker.controller.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const client = require('../classes/pgPoolClass');
 const queries = require('../queries/ticker.queries');
 const tickerLogic = require('../services/ticker');
-
+//GET
 const getTickers = (async (req, res) => {
   try {
     const allTickers = await client.query(queries.getAllTickers);
@@ -26,6 +26,11 @@ const getTickersByChatroomID = (async (req, res) => {
   } catch (err) {
     return res.status(400).send(err);
   };
+});
+
+//INSERT
+const insertTickerByTimeInterval = (async (req, res) => {
+  
 });
 
 module.exports = {
