8962b001910819fa3dfd778204a98009fd9f580e
Stephen <stephen_1996@live.ca>
2023-03-29 13:48:43 -0700

    changed query to not include live chatrooms in conversation

--- server/queries/chat.queries.js
+++ server/queries/chat.queries.js
@@ -2,7 +2,7 @@
 // friends chat
 const getChatroom = "SELECT * FROM chatroom";
 const getChatroomByChatroomID = "SELECT * FROM chatroom WHERE id = $1 AND delete_date IS NULL";
-const getChatroomByUserID = "SELECT participant.account_id, chatroom.name, chatroom.description, participant.chatroom_id FROM participant INNER JOIN chatroom ON participant.chatroom_id=chatroom.id WHERE account_id = $1 AND participant.deleted_at IS NULL AND chatroom.deleted_at IS NULL";
+const getChatroomByUserID = "SELECT participant.account_id, chatroom.name, chatroom.description, participant.chatroom_id FROM participant INNER JOIN chatroom ON participant.chatroom_id=chatroom.id WHERE account_id = $1 AND participant.deleted_at IS NULL AND chatroom.deleted_at IS NULL AND chatroom.live IS NULL";
 const createChatroom = "INSERT INTO chatroom (name, description) VALUES ($1, $2) returning id";
 const deleteChatroom = "UPDATE chatroom SET deleted_at = $1 WHERE id = $2;";
 // live chatrooms for tickers
