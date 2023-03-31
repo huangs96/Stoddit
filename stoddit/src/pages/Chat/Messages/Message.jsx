b9958e5158361b67dc89c7ab60d026b120ab29b6
Stephen <stephen_1996@live.ca>
2023-03-30 17:15:20 -0700

    message layout is finished. Need to adjust font colors

--- stoddit/src/pages/Chat/Messages/Message.jsx
+++ stoddit/src/pages/Chat/Messages/Message.jsx
@@ -15,59 +15,59 @@ function Message({userID, messages}) {
             data.ownMessage = false;
           };
           return (
-            // <div className={data.ownMessage ? "message own" : "message"}> 
-            //   <div 
-            //     className="messageInfo"
-            //     key={i}
-            //   >
-            //     <div className="nameTimestampContainer">
-            //       <span STYLE="margin-right: 10px">
-            //         {data.username}
-            //       </span>
-            //       <span STYLE="color:gray">
-            //         {format(data.sent_at)}
-            //       </span>
-            //     </div>
-            //     <div 
-            //       className={data.ownMessage ? "messageTop own" : "messageTop"}
-            //       >
-            //       <img 
-            //         className="messageImg" 
-            //         src={data.imgUrl}
-            //         alt=""
-            //         />
-            //       <p 
-            //         className="messageText" 
-            //         key={i}
-            //         >
-            //         {data.message_text}
-            //       </p>
-            //     </div>
-            //   </div>
-            // </div>
-            <div className="messageContainer">
-              <img 
-                className="messageImg" 
-                src={data.imgUrl}
-                alt=""
-              />
-              <div className="messageInfoContainer">
-                <div className="nameTimeContainer">
-                  <span>
+            <div className={data.ownMessage ? "message own" : "message"}> 
+              <div 
+                className="messageInfo"
+                key={i}
+              >
+                <div className="nameTimestampContainer">
+                  <span STYLE="margin-right: 10px">
                     {data.username}
                   </span>
-                  <span>
+                  <span className="timestamp">
                     {format(data.sent_at)}
                   </span>
                 </div>
+                <div 
+                  className={data.ownMessage ? "messageTop own" : "messageTop"}
+                  >
+                  <img 
+                    className="messageImg" 
+                    src={data.imgUrl}
+                    alt=""
+                    />
                   <p 
                     className="messageText" 
                     key={i}
                     >
                     {data.message_text}
                   </p>
+                </div>
               </div>
             </div>
+            // <div className="messageContainer">
+            //   <img 
+            //     className="messageImg" 
+            //     src={data.imgUrl}
+            //     alt=""
+            //   />
+            //   <div className="messageInfoContainer">
+            //     <div className="nameTimeContainer">
+            //       <span>
+            //         {data.username}
+            //       </span>
+            //       <span className="timestamp">
+            //         {format(data.sent_at)}
+            //       </span>
+            //     </div>
+            //       <p 
+            //         className="messageText1" 
+            //         key={i}
+            //         >
+            //         {data.message_text}
+            //       </p>
+            //   </div>
+            // </div>
           )
         })
       }
