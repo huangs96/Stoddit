397a051328cd25f079cc9dfcd5f3c821570f908e
Stephen <stephen_1996@live.ca>
2023-04-07 13:32:03 -0700

    added padding to deposits to create more space in deposits block

--- stoddit/src/pages/Home/Dashboard/Title.js
+++ stoddit/src/pages/Home/Dashboard/Title.js
@@ -6,7 +6,7 @@ import Typography from '@mui/material/Typography';
 
 function Title(props) {
   return (
-    <Typography component="h2" variant="h6" color="primary" gutterBottom>
+    <Typography component="h2" variant="h6" color="#4353FF" gutterBottom>
       {props.children}
     </Typography>
   );
