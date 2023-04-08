878dd47b87d4f1ee34af15e4c7b129c7c7ccf1b1
Stephen <stephen_1996@live.ca>
2023-04-08 02:34:31 -0700

    changing color of doughnut chart to match app colour

--- stoddit/src/components/BalanceChart.jsx
+++ stoddit/src/components/BalanceChart.jsx
@@ -4,19 +4,37 @@ import { Doughnut } from 'react-chartjs-2';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-export const data = {
+const options = {
+  legend: {
+    display: false
+  }
+};
+const data = {
   labels: ['investments', 'cash'],
   datasets: [
     {
       label: 'Balance Breakdown',
       data: [1000, 2024],
+      backgroundColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)'
+      ],
       borderWidth: 1,
     },
   ],
+  options: {
+    legend: {
+      display: false
+    }
+  }
 };
 
 export default function BalanceChart() {
   return (
-    <Doughnut data={data} />
+    <Doughnut data={data} options={options} />
   );
 };
