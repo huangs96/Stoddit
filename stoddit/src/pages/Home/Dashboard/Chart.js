8bedf2bf066926cb919d921849f70bb0142c7c40
Stephen <stephen_1996@live.ca>
2023-04-09 01:37:56 -0700

    chart styling in progress, displaying data is working

--- stoddit/src/pages/Home/Dashboard/Chart.js
+++ stoddit/src/pages/Home/Dashboard/Chart.js
@@ -22,25 +22,12 @@ ChartJS.register(
   Filler,
 );
 
-// Generate Sales Data
-function createData(time, amount) {
-  return { time, amount };
-}
-
 const data = [
-  createData('00:00', 0),
-  createData('03:00', 300),
-  createData('06:00', 600),
-  createData('09:00', 800),
-  createData('12:00', 1500),
-  createData('15:00', 2000),
-  createData('18:00', 2400),
-  createData('21:00', 2400),
-  createData('24:00', undefined),
+  3012, 1111, 2428, 2900, 1880, 2500, 3042, 3024
 ];
 
 const liveData = {
-  labels: ['00:00'],
+  labels: ['8:30am', '9:00am', '9:30am', '10:00am', '10:30am', '11:00am'],
   datasets: [{
     label: 'Balance',
     data: data,
@@ -73,52 +60,8 @@ export default function Chart() {
   return (
       <Line
         data={liveData}
-        // data={liveData}
         options={options}
       >
       </Line>
-    // <React.Fragment>
-    //   <Title>Today</Title>
-    //   <ResponsiveContainer>
-    //     <LineChart
-    //       data={data}
-    //       margin={{
-    //         top: 16,
-    //         right: 16,
-    //         bottom: 0,
-    //         left: 24,
-    //       }}
-    //     >
-    //       <XAxis
-    //         dataKey="time"
-    //         stroke={theme.palette.text.secondary}
-    //         style={theme.typography.body2}
-    //       />
-    //       <YAxis
-    //         stroke={theme.palette.text.secondary}
-    //         style={theme.typography.body2}
-    //       >
-    //         <Label
-    //           angle={270}
-    //           position="left"
-    //           style={{
-    //             textAnchor: 'middle',
-    //             fill: theme.palette.text.primary,
-    //             ...theme.typography.body1,
-    //           }}
-    //         >
-    //           Balance ($)
-    //         </Label>
-    //       </YAxis>
-    //       <Line
-    //         isAnimationActive={false}
-    //         type="monotone"
-    //         dataKey="amount"
-    //         stroke={theme.palette.primary.main}
-    //         dot={false}
-    //       />
-    //     </LineChart>
-    //   </ResponsiveContainer>
-    // </React.Fragment>
   );
 }
